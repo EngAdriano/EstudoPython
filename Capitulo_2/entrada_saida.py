b1fc4bb8b47a449512450707491dffd1ff46289b
José Adriano <adriano.liukin@gmail.com>
2025-08-13 09:33:36 -0300

    Atualização

---
+++ Capitulo_2/entrada_saida.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+print(f"Olá, {nome}! Você tem {idade} anos.")
+print("Obrigado por fornecer suas informações!")
+# Este é um exemplo simples de entrada e saída em Python.
+# O código solicita ao usuário que insira seu nome e idade, e então exibe uma mensagem personalizada.
+# Você pode executar este código em um ambiente Python para ver como funciona.
